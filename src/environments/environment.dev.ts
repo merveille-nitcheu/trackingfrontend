756008c336e400db3812acbcac92f329e42cd6f7
merveille <merveillenitcheu12@gmail.com>
2024-08-20 14:29:10 +0100

    update env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
     production: true,
-    apiUrl: "http://lightgroup.co.com:8010/api",
-    pusherAuth: "http://lightgroup.co.com:8010/broadcasting/auth",
+    apiUrl: "http://173.249.8.175:9010/api",
+    pusherAuth: "http://173.249.8.175:9010/broadcasting/auth",
     PUSHER_APP_ID: "1836393",
     PUSHER_APP_KEY: "eb6482817f6d7d49ba0c",
     PUSHER_APP_SECRET: "ecfacbf8fae1ad8f6ef3",
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "http://lightgroup.co.com:8010/api",
-  pusherAuth: "http://lightgroup.co.com:8010/broadcasting/auth",
+  apiUrl: "http://173.249.8.175:9010/api",
+  pusherAuth: "http://173.249.8.175:9010/broadcasting/auth",
   PUSHER_APP_ID: "1836392",
   PUSHER_APP_KEY: "161627330028cfb8922e",
   PUSHER_APP_SECRET: "3aa7691ac87bdf61bb1e",
